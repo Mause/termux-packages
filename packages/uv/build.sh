0c4049ede3e66b59f032a8af06f15e47dfb30f75
Elliana May <me@mause.me>
2024-04-16 16:22:12 +0800

    Update build.sh

--- packages/uv/build.sh
+++ packages/uv/build.sh
@@ -8,6 +8,7 @@ TERMUX_PKG_HOMEPAGE="https://pypi.org/project/uv/"
 TERMUX_PKG_SHA256="339a275711f2c1faab155f47c2e06e272292401ab054ede37d92ef7ebbffad9b"
 TERMUX_PKG_SRCURL="https://files.pythonhosted.org/packages/57/94/97d0f47c8dbc1c0f6fbfaa3b39d3686220b2199a762735d364d5ba942031/uv-${TERMUX_PKG_VERSION}.tar.gz"
 TERMUX_PKG_PYTHON_COMMON_DEPS="maturin"
+TERMUX_PKG_SETUP_PYTHON=true
 
 termux_step_pre_configure() {
 	termux_setup_rust
