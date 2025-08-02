0a78c888d29dd4d237d40b6a48f9cc70ca0edbc8
Elliana May <me@mause.me>
2025-08-02 11:34:25 +1000

    try building using pip instead

--- packages/ty/build.sh
+++ packages/ty/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://docs.astral.sh/ty/
 TERMUX_PKG_DESCRIPTION="An extremely fast Python typechecker, written in Rust."
 TERMUX_PKG_LICENSE="MIT"
-TERMUX_PKG_LICENSE_FILE="../../LICENSE"
+TERMUX_PKG_LICENSE_FILE="../../../LICENSE"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="0.0.1-alpha.28"
 TERMUX_PKG_SRCURL=https://github.com/astral-sh/ty/releases/download/${TERMUX_PKG_VERSION}/source.tar.gz
@@ -11,11 +11,16 @@ TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
 
 termux_step_post_get_source() {
-	TERMUX_PKG_SRCDIR+="/crates/ty"
+	TERMUX_PKG_SRCDIR+="/ruff/crates/ty"
+
+	local file="$TERMUX_PKG_CACHEDIR/$(basename "${TERMUX_PKG_SRCURL}")"
+	mkdir -p "$TERMUX_PKG_SRCDIR"
+	tar xf "$file" -C "$TERMUX_PKG_SRCDIR" --strip-components=0
 }
 
 termux_step_pre_configure() {
 	termux_setup_rust
+	termux_setup_python_pip
 
 	# Dummy CMake toolchain file to workaround build error:
 	# error: failed to run custom build command for `libz-ng-sys v1.1.15`
@@ -29,16 +34,6 @@ termux_step_pre_configure() {
 	export CARGO_HOME
 }
 
-termux_step_make() {
-	cd "$TERMUX_PKG_SRCDIR"
-
-	PKG_CONFIG_PATH="/usr/lib/x86_64-linux-gnu/pkgconfig/" \
-	PKG_CONFIG_ALL_DYNAMIC=1 \
-	ZSTD_SYS_USE_PKG_CONFIG=1 \
-	TY_VERSION="${TERMUX_PKG_VERSION}" \
-	cargo build --jobs "${TERMUX_PKG_MAKE_PROCESSES}" --target "${CARGO_TARGET_NAME}" --release --verbose
-}
-
 termux_step_make_install() {
-	install -Dm700 -t "${TERMUX_PREFIX}"/bin target/"${CARGO_TARGET_NAME}"/release/ty
+	pip install .
 }
