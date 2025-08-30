f74c0d079374c2fb0c1ca20b4c98af5eadba279f
Elliana May <me@mause.me>
2025-08-30 20:55:25 +0800

    add whisper.cpp

---
+++ packages/whisper.cpp/build.sh
@@ -0,0 +1,17 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/ggml-org/whisper.cpp
+TERMUX_PKG_DESCRIPTION="whisper.cpp"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="1.7.6"
+TERMUX_PKG_AUTO_UPDATE=true
+# we clone to retain the .git directory, to ensure the version in the built executable is correctly populated
+TERMUX_PKG_SRCURL=git+http://github.com/ggml-org/whisper.cpp
+#TERMUX_PKG_DEPENDS="libc++, openssl"
+#TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DBUILD_EXTENSIONS='icu;parquet;json;autocomplete' -DDUCKDB_EXPLICIT_PLATFORM=linux_arm64_android"
+
+# termux_step_pre_configure() {
+# 	LDFLAGS+=" -llog"
+# 	CXXFLAGS+=" -D_GLIBCXX_USE_CXX11_ABI=1"
+# }
+
+
