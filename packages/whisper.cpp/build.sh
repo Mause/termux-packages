cdbe158878b82147fba9cbad1e20dc740ab1c3f3
Elliana May <me@mause.me>
2025-08-30 21:04:04 +0800

    lint

--- packages/whisper.cpp/build.sh
+++ packages/whisper.cpp/build.sh
@@ -5,7 +5,7 @@ TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="1.7.6"
 TERMUX_PKG_AUTO_UPDATE=true
 # we clone to retain the .git directory, to ensure the version in the built executable is correctly populated
-TERMUX_PKG_SRCURL=git+http://github.com/ggml-org/whisper.cpp
+TERMUX_PKG_SRCURL=git+https://github.com/ggml-org/whisper.cpp
 #TERMUX_PKG_DEPENDS="libc++, openssl"
 #TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DBUILD_EXTENSIONS='icu;parquet;json;autocomplete' -DDUCKDB_EXPLICIT_PLATFORM=linux_arm64_android"
 
@@ -13,5 +13,4 @@ TERMUX_PKG_SRCURL=git+http://github.com/ggml-org/whisper.cpp
 # 	LDFLAGS+=" -llog"
 # 	CXXFLAGS+=" -D_GLIBCXX_USE_CXX11_ABI=1"
 # }
-
 
