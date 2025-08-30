28020b3a354812cc6ca8983570aae958fe81994d
Elliana May <me@mause.me>
2025-08-30 15:39:20 +0800

    bump

--- packages/ffmpeg/build.sh
+++ packages/ffmpeg/build.sh
@@ -17,7 +17,7 @@ termux_step_pre_configure() {
 	# Do not forget to bump revision of reverse dependencies and rebuild them
 	# after SOVERSION is changed. (These variables are also used afterwards.)
 	_FFMPEG_SOVER_avutil=60
-	_FFMPEG_SOVER_avcodec=61
+	_FFMPEG_SOVER_avcodec=62
 	_FFMPEG_SOVER_avformat=61
 
 	local f
