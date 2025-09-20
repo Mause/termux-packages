77c1c6cce366cc059c58e6ce19075535afa09d8f
Elliana May <me@mause.me>
2025-09-20 12:20:10 +0800

    git-absorb

---
+++ packages/git-absorb/build.sh
@@ -0,0 +1,39 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/tummychow/git-absorb
+TERMUX_PKG_DESCRIPTION="git commit --fixup, but automatic"
+TERMUX_PKG_LICENSE="BSD3"
+TERMUX_PKG_LICENSE_FILE="LICENSE.md"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="0.8.0"
+TERMUX_PKG_SRCURL=https://github.com/tummychow/git-absorb/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=43064d198163c0b027f9167bb3422078ca0ed04bf3b9892c6a276f81b230de23
+# TERMUX_PKG_DEPENDS="zstd"
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_AUTO_UPDATE=true
+
+termux_step_pre_configure() {
+	termux_setup_rust
+
+	# Dummy CMake toolchain file to workaround build error:
+	# error: failed to run custom build command for `libz-ng-sys v1.1.15`
+	# ...
+	# CMake Error at /home/builder/.termux-build/_cache/cmake-3.28.3/share/cmake-3.28/Modules/Platform/Android-Determine.cmake:217 (message):
+	# Android: Neither the NDK or a standalone toolchain was found.
+	export TARGET_CMAKE_TOOLCHAIN_FILE="${TERMUX_PKG_BUILDDIR}/android.toolchain.cmake"
+	touch "${TERMUX_PKG_BUILDDIR}/android.toolchain.cmake"
+
+	: "${CARGO_HOME:=$HOME/.cargo}"
+	export CARGO_HOME
+}
+
+termux_step_make() {
+	cd "$TERMUX_PKG_SRCDIR"
+
+	PKG_CONFIG_PATH="/usr/lib/x86_64-linux-gnu/pkgconfig/" \
+	PKG_CONFIG_ALL_DYNAMIC=1 \
+	ZSTD_SYS_USE_PKG_CONFIG=1 \
+	cargo build --jobs "${TERMUX_PKG_MAKE_PROCESSES}" --target "${CARGO_TARGET_NAME}" --release --verbose
+}
+
+termux_step_make_install() {
+	install -Dm700 -t "${TERMUX_PREFIX}"/bin target/"${CARGO_TARGET_NAME}"/release/git-aborb
+}
