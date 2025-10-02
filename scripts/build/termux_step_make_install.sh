e1d169d7fbbc5795dea0627a235f92cc24f1c995
Elliana May <me@mause.me>
2025-10-02 22:09:12 +0800

    Replace pip wheel with pipx cibuildwheel for installation

--- scripts/build/termux_step_make_install.sh
+++ scripts/build/termux_step_make_install.sh
@@ -5,7 +5,7 @@ termux_step_make_install() {
 	if test -f build.ninja; then
 		ninja -j $TERMUX_PKG_MAKE_PROCESSES install
 	elif test -f setup.py || test -f pyproject.toml || test -f setup.cfg; then
-		pip wheel --no-deps . --wheel-dir $TERMUX_PREFIX
+		pipx cibuildwheel . --wheel-dir $TERMUX_PREFIX
 		SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 		python3 $SCRIPT_DIR/../check_tags.py $TERMUX_PREFIX
 	elif ls ./*.cabal &>/dev/null || ls ./cabal.project &>/dev/null; then
