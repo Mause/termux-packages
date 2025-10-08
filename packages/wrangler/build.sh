538ecf8703df70f4c96530b5c30bb89c805e2601
Elliana May <me@mause.me>
2025-10-08 16:15:19 +0800

    verbose

--- packages/wrangler/build.sh
+++ packages/wrangler/build.sh
@@ -11,6 +11,8 @@ TERMUX_PKG_DEPENDS="nodejs"
 TERMUX_PKG_SHA256=0c0477663aaa85fcf2f798581529a9cdacdb14f5d093dd5b861a854c7b056e05
 TERMUX_PKG_BUILD_IN_SRC=true
 
+set -o xtrace
+
 termux_step_post_get_source() {
 	TERMUX_PKG_SRCDIR+="/packages/wrangler"
 }
