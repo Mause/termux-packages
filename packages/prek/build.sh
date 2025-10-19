1872a2ae914051778ad66d658aeb3e8e3cefec00
Elliana May <me@mause.me>
2025-10-19 15:00:04 +0800

    add prek

---
+++ packages/prek/build.sh
@@ -0,0 +1,9 @@
+TERMUX_PKG_HOMEPAGE='https://github.com/j178/prek'
+TERMUX_PKG_DESCRIPTION='⚡ Better `pre-commit`, re-engineered in Rust'
+TERMUX_PKG_LICENSE='MIT'
+TERMUX_PKG_MAINTAINER='@termux'
+TERMUX_PKG_VERSION='0.2.9'
+TERMUX_PKG_SRCURL=https://github.com/j178/prek/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256='0fd25048c1498e3a54f7847fc6fc5119a6201c8c998f13d2743fd99795bac260'
+TERMUX_PKG_BUILD_IN_SRC='true'
+TERMUX_PKG_AUTO_UPDATE='true'
