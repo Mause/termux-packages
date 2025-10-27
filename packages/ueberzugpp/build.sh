e525736d78a3bb296361374f30393001d4b8e3c3
Elliana May <me@mause.me>
2025-10-27 21:33:43 +0800

    addpkg(main/ueberzugpp): v2.9.7

---
+++ packages/ueberzugpp/build.sh
@@ -0,0 +1,9 @@
+TERMUX_PKG_HOMEPAGE='https://github.com/jstkdng/ueberzugpp'
+TERMUX_PKG_DESCRIPTION='Drop in replacement for ueberzug written in C++'
+TERMUX_PKG_LICENSE='GPL-3.0'
+TERMUX_PKG_MAINTAINER='@termux'
+TERMUX_PKG_VERSION='2.9.7'
+TERMUX_PKG_SRCURL=https://github.com/jstkdng/ueberzugpp/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256='fcf4b35e2a38a42a9ea6665f2168a1f778008424b14817bcd381dbb7b5cc54b7'
+TERMUX_PKG_BUILD_IN_SRC='true'
+TERMUX_PKG_AUTO_UPDATE='true'
