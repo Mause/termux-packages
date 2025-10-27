cea0ed207a337045ce99eda98eb0597243e431c1
Elliana May <me@mause.me>
2025-10-27 21:52:52 +0800

    libchafa -> chafa

--- packages/ueberzugpp/build.sh
+++ packages/ueberzugpp/build.sh
@@ -7,4 +7,4 @@ TERMUX_PKG_SRCURL=https://github.com/jstkdng/ueberzugpp/archive/refs/tags/v${TER
 TERMUX_PKG_SHA256='fcf4b35e2a38a42a9ea6665f2168a1f778008424b14817bcd381dbb7b5cc54b7'
 TERMUX_PKG_BUILD_IN_SRC='true'
 TERMUX_PKG_AUTO_UPDATE='true'
-TERMUX_PKG_DEPENDS='openssl, libvips, libsixel, libchafa, libtbb'
+TERMUX_PKG_DEPENDS='openssl, libvips, libsixel, chafa, libtbb'
