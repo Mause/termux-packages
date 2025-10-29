b914ddc2758d15d09f15d9f38c983f373d2f6ee3
Elliana May <me@mause.me>
2025-10-30 00:32:40 +0800

    addpkg(main/ghostty): 1.2.3

---
+++ packages/ghostty/build.sh
@@ -0,0 +1,9 @@
+TERMUX_PKG_HOMEPAGE='https://github.com/ghostty-org/ghostty'
+TERMUX_PKG_DESCRIPTION='👻 Ghostty is a fast, feature-rich, and cross-platform terminal emulator that uses platform-native UI and GPU acceleration.'
+TERMUX_PKG_LICENSE='MIT'
+TERMUX_PKG_MAINTAINER='@termux'
+TERMUX_PKG_VERSION='1.2.3'
+TERMUX_PKG_SRCURL=https://github.com/ghostty-org/ghostty/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256='e17d39482fc70fba3d72f5f25c12e9d9a72b87dd45a61a854d9928e98b69edd8'
+TERMUX_PKG_BUILD_IN_SRC='true'
+TERMUX_PKG_AUTO_UPDATE='true'
