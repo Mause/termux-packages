9de0efafd54414328692f3a29e1776c351a0e34d
Elliana May <me@mause.me>
2025-11-02 23:12:56 +0800

    set commit

--- packages/tsgo/build.sh
+++ packages/tsgo/build.sh
@@ -1,15 +1,20 @@
 TERMUX_PKG_DESCRIPTION="A port of the typescript compiler to go"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_GIT_BRANCH=main
-TERMUX_PKG_VERSION="0.1.0"
+TERMUX_PKG_VERSION="0.1.1"
 TERMUX_PKG_HOMEPAGE="https://github.com/microsoft/typescript-go"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_LICENSE_FILE="LICENSE"
 TERMUX_PKG_SRCURL="git+https://github.com/microsoft/typescript-go"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_GIT_COMMIT="6fb55b7eabb48336d58a4ead49df12e91e386a15"
 
 set -o xtrace
+
+termux_post_get_source() {
+	git checkout "$TERMUX_PKG_GIT_COMMIT"
+}
 
 termux_step_pre_configure() {
 	termux_setup_rust
