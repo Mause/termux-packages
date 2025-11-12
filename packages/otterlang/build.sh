6ae3cfa0e811d2e918d2ccdc4fe562d40a1c0f07
Elliana May <me@mause.me>
2025-11-12 23:44:28 +0800

    add nightly target

--- packages/otterlang/build.sh
+++ packages/otterlang/build.sh
@@ -20,5 +20,6 @@ termux_step_post_get_source() {
 termux_step_make() {
 	termux_setup_rust
 	. $HOME/.cargo/env
+	rustup target add nightly-aarch64-linux-android
 	cargo +nightly install --jobs 4 --path . --force --locked --no-track --target $CARGO_TARGET_NAME --root $TERMUX_PREFIX
 }
