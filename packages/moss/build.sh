94f59711077dbe42aa2fe594413aad0e809f1218
Elliana May <me@mause.me>
2025-11-21 20:53:31 +0800

    addpkg(main/moss): 0.0.1

---
+++ packages/moss/build.sh
@@ -0,0 +1,14 @@
+TERMUX_PKG_HOMEPAGE='https://github.com/hexagonal-sun/moss'
+TERMUX_PKG_DESCRIPTION='Rust Linux-compatible kernel'
+TERMUX_PKG_LICENSE='Codehaus'
+TERMUX_PKG_MAINTAINER='@termux'
+TERMUX_PKG_VERSION='0.0.1'
+TERMUX_PKG_SRCURL=git+https://github.com/hexagonal-sun/moss
+_COMMIT='fae19e3cba73d2ad65651167a76e92c9802853a3'
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_AUTO_UPDATE=true
+
+termux_step_post_get_source() {
+	git fetch --unshallow
+	git checkout $_COMMIT
+}
