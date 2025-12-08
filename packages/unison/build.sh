783c0a8df668587fc23c331ef2eb4fec3adce2e3
Elliana May <me@mause.me>
2025-12-08 12:56:16 +0800

    stack

--- packages/unison/build.sh
+++ packages/unison/build.sh
@@ -9,9 +9,12 @@ TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
 
 termux_step_pre_configure() {
-	termux_setup_cabal
+	# termux_setup_cabal
 	termux_setup_ghc
+	curl -sSL https://get.haskellstack.org/ | sh
 }
 termux_step_make() {
-	cabal --config="$TERMUX_CABAL_CONFIG" build exe:hledger-ui
+	# cabal --config="$TERMUX_CABAL_CONFIG" build exe:hledger-ui
+	stack --version # we'll want to know this version if you run into trouble
+	stack build --fast --test && stack exec unison
 }
