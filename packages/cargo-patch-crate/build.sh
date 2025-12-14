39a4e27f79e3ad60d5a5dcc9ae04bdeb863c567f
Elliana May <me@mause.me>
2025-12-15 00:27:47 +0800

    use commit

--- packages/cargo-patch-crate/build.sh
+++ packages/cargo-patch-crate/build.sh
@@ -4,10 +4,16 @@ TERMUX_PKG_LICENSE='MIT'
 TERMUX_PKG_MAINTAINER='@termux'
 TERMUX_PKG_VERSION='dc063b222854f6474950f26d17e5ca09b9074c56'
 TERMUX_PKG_SRCURL=https://github.com/mokeyish/cargo-patch-crate/archive/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256='d626e01f31cceb4177195a392345a8b8631ac627c0f4e682b65a36e1e2ecdd47'
+TERMUX_PKG_SHA256='0.0.1'
+_COMMIT='d626e01f31cceb4177195a392345a8b8631ac627c0f4e682b65a36e1e2ecdd47'
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
 
 termux_step_pre_configure() {
 	termux_setup_rust
+}
+
+termux_step_post_get_source() {
+    git fetch --unshallow
+	git checkout $_COMMIT
 }
