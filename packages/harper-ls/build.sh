8d752e66a286d2d7aecd4d9431e68b2bd50dbac1
Elliana May <me@mause.me>
2025-12-30 12:34:28 +0800

    addpkg(main/harper-ls): 1.3.0

---
+++ packages/harper-ls/build.sh
@@ -0,0 +1,13 @@
+TERMUX_PKG_HOMEPAGE='https://github.com/Automattic/harper'
+TERMUX_PKG_DESCRIPTION='Offline, privacy-first grammar checker. Fast, open-source, Rust-powered'
+TERMUX_PKG_LICENSE='Apache-2.0'
+TERMUX_PKG_MAINTAINER='@termux'
+TERMUX_PKG_VERSION='1.3.0'
+TERMUX_PKG_SRCURL=https://github.com/Automattic/harper/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_SHA256='ccc408a4f9124cacfdb76bbf9aa42a5950b2cc6acebadca9c8dd0f02c70f4104'
+
+termux_step_pre_configure() {
+	termux_setup_rust
+}
