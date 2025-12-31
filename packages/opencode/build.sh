24487d08249924573e3cce069fd5be469096c2fc
Elliana May <me@mause.me>
2026-01-01 01:25:29 +0800

    bun

--- packages/opencode/build.sh
+++ packages/opencode/build.sh
@@ -7,3 +7,8 @@ TERMUX_PKG_SRCURL=https://github.com/sst/opencode/archive/refs/tags/v${TERMUX_PK
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_SHA256='2ec05c65e06947df238bf534c53a0b2a72e23d6303883fdb242263d5c9935a79'
+
+termux_step_make() {
+	curl -fsSL https://bun.com/install | bash
+	~/.bun/bin/bun install --production .
+}
