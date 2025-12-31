ba4a165fdb3dc0a9525604f10407bd737b9b1baa
Elliana May <me@mause.me>
2026-01-01 01:30:41 +0800

    build exe

--- packages/opencode/build.sh
+++ packages/opencode/build.sh
@@ -10,5 +10,8 @@ TERMUX_PKG_SHA256='2ec05c65e06947df238bf534c53a0b2a72e23d6303883fdb242263d5c9935
 
 termux_step_make() {
 	curl -fsSL https://bun.com/install | bash
-	~/.bun/bin/bun install --production .
+	source /home/builder/.bashrc
+
+	bun install
+	./packages/opencode/script/build.ts --single
 }
