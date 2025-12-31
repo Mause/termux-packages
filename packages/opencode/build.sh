89e6d592f224dc996c5a557c38f5dd8f3665b6f2
Elliana May <me@mause.me>
2026-01-01 01:23:22 +0800

    addpkg(main/opencode): 1.0.219

---
+++ packages/opencode/build.sh
@@ -0,0 +1,9 @@
+TERMUX_PKG_HOMEPAGE='https://github.com/sst/opencode'
+TERMUX_PKG_DESCRIPTION='The open source coding agent.'
+TERMUX_PKG_LICENSE='MIT'
+TERMUX_PKG_MAINTAINER='@termux'
+TERMUX_PKG_VERSION='1.0.219'
+TERMUX_PKG_SRCURL=https://github.com/sst/opencode/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_SHA256='2ec05c65e06947df238bf534c53a0b2a72e23d6303883fdb242263d5c9935a79'
