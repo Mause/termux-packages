f8f7fc1e3dfc94ac7c0185a7bacf85000e473b69
Elliana May <me@mause.me>
2026-01-07 13:12:20 +0700

    Add fontconfig as a dependency for binwalk

--- packages/binwalk/build.sh
+++ packages/binwalk/build.sh
@@ -7,6 +7,7 @@ TERMUX_PKG_SRCURL=https://github.com/ReFirmLabs/binwalk/archive/refs/tags/v${TER
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_SHA256='06f595719417b70a592580258ed980237892eadc198e02363201abe6ca59e49a'
+TERMUX_PKG_DEPENDS='fontconfig'
 
 termux_step_pre_configure() {
 	termux_setup_rust
