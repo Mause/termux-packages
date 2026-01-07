baddd9959483a9c2a649d8dc46589dba8b0daa40
Elliana May <me@mause.me>
2026-01-07 22:21:55 +0700

    tidy

--- packages/aws-crt-python/build.sh
+++ packages/aws-crt-python/build.sh
@@ -4,17 +4,11 @@ TERMUX_PKG_LICENSE='Apache-2.0'
 TERMUX_PKG_MAINTAINER='@termux'
 TERMUX_PKG_VERSION='0.31.0'
 TERMUX_PKG_SRCURL=git+https://github.com/awslabs/aws-crt-python
-TERMUX_PKG_GIT_BRANCH=v${TERMUX_PKG_VERSION}
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="python, openssl"
 
 set -o xtrace
-
-termux_step_post_get_source() {
-	git fetch --unshallow
-	git submodule init
-}
 
 termux_step_pre_configure() {
 	termux_setup_python_pip
