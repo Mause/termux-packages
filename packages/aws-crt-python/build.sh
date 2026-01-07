216d5abd23aa2521ef1c897c98e31a4f7cd9880d
Elliana May <me@mause.me>
2026-01-07 21:40:49 +0800

    get submodules

--- packages/aws-crt-python/build.sh
+++ packages/aws-crt-python/build.sh
@@ -3,10 +3,15 @@ TERMUX_PKG_DESCRIPTION='Python bindings for the AWS Common Runtime'
 TERMUX_PKG_LICENSE='Apache-2.0'
 TERMUX_PKG_MAINTAINER='@termux'
 TERMUX_PKG_VERSION='0.31.0'
-TERMUX_PKG_SRCURL=https://github.com/awslabs/aws-crt-python/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SRCURL=git+https://github.com/awslabs/aws-crt-python
+TERMUX_PKG_GIT_BRANCH=v${TERMUX_PKG_VERSION}
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_SHA256='b429ae666e6c1ab9aadb31e00c3556f1dcf363828cf1d04e63f8207f10b28096'
+
+termux_step_post_get_source() {
+	git fetch --unshallow
+	git submodule init
+}
 
 termux_step_pre_configure() {
 	termux_setup_python_pip
