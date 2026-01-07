f6f28c0a7e524a2152a1b19d7d1177f7da54ee7e
Elliana May <me@mause.me>
2026-01-07 21:45:05 +0700

    Add aws-crt-python as a dependency

--- packages/aws-cli/build.sh
+++ packages/aws-cli/build.sh
@@ -7,7 +7,7 @@ TERMUX_PKG_SRCURL=https://github.com/aws/aws-cli/archive/refs/tags/${TERMUX_PKG_
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_SHA256='8318ec37c35b4cac817369a4d41740f54fa83c62611d84c65eb7204e77f7dd85'
-TERMUX_PKG_DEPENDS='python, openssl'
+TERMUX_PKG_DEPENDS='python, openssl, ‎aws-crt-python'
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS='--with-download-deps'
 
 set -o xtrace
