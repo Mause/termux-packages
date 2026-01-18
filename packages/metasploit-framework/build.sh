d18ed0cd2cf53ba6d1c1ffb7e58e28fd83643847
Elliana May <me@mause.me>
2026-01-18 11:23:23 +0700

    license

--- packages/metasploit-framework/build.sh
+++ packages/metasploit-framework/build.sh
@@ -1,6 +1,6 @@
 TERMUX_PKG_HOMEPAGE='https://github.com/rapid7/metasploit-framework/'
 TERMUX_PKG_DESCRIPTION='Metasploit Framework'
-TERMUX_PKG_LICENSE=''
+TERMUX_PKG_LICENSE='BSD 3-Clause'
 TERMUX_PKG_MAINTAINER='@termux'
 TERMUX_PKG_VERSION='4.11.7'
 TERMUX_PKG_SRCURL=https://github.com/rapid7/metasploit-framework/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
