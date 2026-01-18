9afdc585eb6e366006795a907249055bbb51cbb7
Elliana May <me@mause.me>
2026-01-18 10:54:51 +0700

    addpkg(main/metasploit-framework): 4.11.7

---
+++ packages/metasploit-framework/build.sh
@@ -0,0 +1,9 @@
+TERMUX_PKG_HOMEPAGE='https://github.com/rapid7/metasploit-framework/'
+TERMUX_PKG_DESCRIPTION='Metasploit Framework'
+TERMUX_PKG_LICENSE=''
+TERMUX_PKG_MAINTAINER='@termux'
+TERMUX_PKG_VERSION='4.11.7'
+TERMUX_PKG_SRCURL=https://github.com/rapid7/metasploit-framework/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_SHA256='409600db0fb2903cb7f029a9fbddee984373e9cef7b451526bf79e7d3b2fba36'
