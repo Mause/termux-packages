14e0a183db7f0112ec9fdcc31c0239fc0bb7e674
Elliana May <me@mause.me>
2026-02-15 04:03:20 +0800

    addpkg(main/tokenizers): 0.22.2

---
+++ packages/tokenizers/build.sh
@@ -0,0 +1,13 @@
+TERMUX_PKG_HOMEPAGE='https://github.com/huggingface/tokenizers'
+TERMUX_PKG_DESCRIPTION='💥 Fast State-of-the-Art Tokenizers optimized for Research and Production'
+TERMUX_PKG_LICENSE='Apache-2.0'
+TERMUX_PKG_MAINTAINER='@termux'
+TERMUX_PKG_VERSION='0.22.2'
+TERMUX_PKG_SRCURL=https://github.com/huggingface/tokenizers/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_SHA256='05bffc70e12de04d4c060f9ecd404519aa069e93151c5642e7d731298d9273f6'
+
+termux_step_pre_configure() {
+	termux_setup_rust
+}
