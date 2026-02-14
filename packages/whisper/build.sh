46faed023f7bd386751468a8c3fc8213f29e83a8
Elliana May <me@mause.me>
2026-02-14 12:20:48 +0800

    python-llvmlite

--- packages/whisper/build.sh
+++ packages/whisper/build.sh
@@ -6,7 +6,7 @@ TERMUX_PKG_VERSION='20250625'
 TERMUX_PKG_SRCURL=https://github.com/openai/whisper/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS='python-numpy'
+TERMUX_PKG_DEPENDS='python-numpy, python-llvmlite'
 TERMUX_PKG_SHA256='812e8a4b54e117c1bda827597f54886084bf062453ede274caf4699153b673f9'
 
 termux_step_pre_configure() {
