16d85d05617c8a220fe9db77de437e074583b836
Elliana May <me@mause.me>
2026-02-15 03:55:53 +0800

    Add python-pip as a dependency

--- packages/faster-whisper/build.sh
+++ packages/faster-whisper/build.sh
@@ -6,7 +6,7 @@ TERMUX_PKG_VERSION='1.2.1'
 TERMUX_PKG_SRCURL=https://github.com/guillaumekln/faster-whisper/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="python"
+TERMUX_PKG_DEPENDS="python, python-pip"
 TERMUX_PKG_SHA256='ff0b5428b3a074cd63f580ae73fa21f7d9fd3bba28e0e2e097868d78d5b06f2f'
 
 termux_step_pre_configure() {
