f1207bca0a8bafaa514aeeb7858f434a08099b87
Elliana May <me@mause.me>
2026-02-15 03:40:46 +0800

    addpkg(main/faster-whisper): 1.2.1

---
+++ packages/faster-whisper/build.sh
@@ -0,0 +1,13 @@
+TERMUX_PKG_HOMEPAGE='https://github.com/guillaumekln/faster-whisper'
+TERMUX_PKG_DESCRIPTION='Faster Whisper transcription with CTranslate2'
+TERMUX_PKG_LICENSE='MIT'
+TERMUX_PKG_MAINTAINER='@termux'
+TERMUX_PKG_VERSION='1.2.1'
+TERMUX_PKG_SRCURL=https://github.com/guillaumekln/faster-whisper/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_SHA256='ff0b5428b3a074cd63f580ae73fa21f7d9fd3bba28e0e2e097868d78d5b06f2f'
+
+termux_step_pre_configure() {
+	termux_setup_python_pip
+}
