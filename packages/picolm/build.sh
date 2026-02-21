cd63798f761ed793b5f7bc4bed3457b9d8d8d9be
Elliana May <me@mause.me>
2026-02-21 10:48:15 +1030

    addpkg(main/picolm): 0.0.1

---
+++ packages/picolm/build.sh
@@ -0,0 +1,15 @@
+TERMUX_PKG_HOMEPAGE='https://github.com/RightNow-AI/picolm'
+TERMUX_PKG_DESCRIPTION='Run a 1-billion parameter LLM on a $10 board with 256MB RAM'
+TERMUX_PKG_LICENSE='MIT'
+TERMUX_PKG_MAINTAINER='@termux'
+TERMUX_PKG_VERSION='0.0.1'
+TERMUX_PKG_SRCURL=git+https://github.com/RightNow-AI/picolm
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_AUTO_UPDATE=true
+_COMMIT='20910bcd8694a470c5aebbb1df82434bc00794dc'
+TERMUX_PKG_GIT_BRANCH='main'
+
+termux_step_post_get_source() {
+	git fetch --unshallow
+	git checkout $_COMMIT
+}
