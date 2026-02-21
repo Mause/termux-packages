716b3f04fcae5e918a5b991150cf981652a5ea9d
Elliana May <me@mause.me>
2026-02-21 10:52:19 +1030

    Add debugging output to build script

--- packages/picolm/build.sh
+++ packages/picolm/build.sh
@@ -9,7 +9,14 @@ TERMUX_PKG_AUTO_UPDATE=true
 _COMMIT='20910bcd8694a470c5aebbb1df82434bc00794dc'
 TERMUX_PKG_GIT_BRANCH='main'
 
+set -o xtrace
+
 termux_step_post_get_source() {
 	git fetch --unshallow
 	git checkout $_COMMIT
+}
+
+termux_step_make() {
+	make native
+	make model
 }
