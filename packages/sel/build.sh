b10192c83ddce2b04609226dfd3d5badb9fa7a72
Elliana May <me@mause.me>
2026-02-28 21:57:32 +1100

    dump limits

--- packages/sel/build.sh
+++ packages/sel/build.sh
@@ -26,7 +26,7 @@ termux_step_pre_configure() {
 }
 
 termux_step_make() {
-	echo 20 > /proc/sys/vm/nr_hugepages
+	ulimit -a
 	LISP_HEAP=4000 LISP=./sbcl-$_SBCL-x86-linux/run-sbcl.sh make bin/tree-sitter-interface
 }
 
