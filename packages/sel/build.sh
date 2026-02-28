8fd4c147875744c39ff6ea06c876dbaffaed96ae
Elliana May <me@mause.me>
2026-02-28 20:23:07 +1100

    run without install

--- packages/sel/build.sh
+++ packages/sel/build.sh
@@ -20,9 +20,9 @@ termux_step_pre_configure() {
 	wget http://prdownloads.sourceforge.net/sbcl/sbcl-1.4.3-x86-linux-binary.tar.bz2
 	bzip2 -cd sbcl-1.4.3-x86-linux-binary.tar.bz2 | tar xvf -
 	ls
-	pushd sbcl-1.4.3-x86-linux
-	sh -o xtrace ./install.sh
-	popd
+	# pushd sbcl-1.4.3-x86-linux
+	# sh -o xtrace ./install.sh
+	# popd
 	curl -O https://beta.quicklisp.org/quicklisp.lisp
-	./bin/sbcl --load quicklisp.lisp --eval '(progn (quicklisp-quickstart:install) (ql-util:without-prompting (ql:add-to-init-file)))'
+	./sbcl-1.4.3-x86-linux/run-sbcl.sh --load quicklisp.lisp --eval '(progn (quicklisp-quickstart:install) (ql-util:without-prompting (ql:add-to-init-file)))'
 }
