912bcc12b4a663f201845a1299eace3ca5ce68f1
Elliana May <me@mause.me>
2026-03-14 10:54:34 +0800

    addpkg(main/vite-plus): 0.1.11

---
+++ packages/vite-plus/build.sh
@@ -0,0 +1,13 @@
+TERMUX_PKG_HOMEPAGE='https://github.com/voidzero-dev/vite-plus'
+TERMUX_PKG_DESCRIPTION='Vite+ is the unified toolchain and entry point for web development. It manages your runtime, package manager, and frontend toolchain in one place.'
+TERMUX_PKG_LICENSE='MIT'
+TERMUX_PKG_MAINTAINER='@termux'
+TERMUX_PKG_VERSION='0.1.11'
+TERMUX_PKG_SRCURL=https://github.com/voidzero-dev/vite-plus/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_SHA256='9542f8de994766b2d646566bb788b74a27d6d1f5cd9acb32f10e0a284403cfe1'
+
+termux_step_pre_configure() {
+	termux_setup_rust
+}
