f530bee9534b3d50275300a5f6cf695834796359
Elliana May <me@mause.me>
2026-03-18 20:04:58 +0800

    termux_step_make

--- packages/ilspy/build.sh
+++ packages/ilspy/build.sh
@@ -9,6 +9,22 @@ TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_SHA256='1e69ecea8c4495f6b8ac76162c86ca47b682a70af657a4b24b04bde1430d8ea5'
 TERMUX_PKG_DEPENDS="dotnet-host, dotnet-runtime-8.0"
 
+set -o xtrace
+
 termux_step_pre_configure() {
 	termux_setup_dotnet
+}
+
+termux_step_make() {
+	dotnet publish src/Jackett.Server \
+	--framework "net${TERMUX_DOTNET_VERSION}" \
+	--no-self-contained \
+	--runtime "$DOTNET_TARGET_NAME" \
+	--configuration Release \
+	--output build/ \
+	/p:AssemblyVersion="${TERMUX_PKG_VERSION}" \
+	/p:FileVersion="${TERMUX_PKG_VERSION}" \
+	/p:InformationalVersion="${TERMUX_PKG_VERSION}" \
+	/p:Version="${TERMUX_PKG_VERSION}"
+	dotnet build-server shutdown
 }
