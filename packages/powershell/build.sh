1876fa5cbdebd7816f0678cbfa5066bddd57af2f
Elliana May <me@mause.me>
2026-03-19 15:21:52 +0800

    addpkg(main/powershell): 7.6.0

---
+++ packages/powershell/build.sh
@@ -0,0 +1,9 @@
+TERMUX_PKG_HOMEPAGE='https://github.com/PowerShell/PowerShell'
+TERMUX_PKG_DESCRIPTION='PowerShell for every system!'
+TERMUX_PKG_LICENSE='MIT'
+TERMUX_PKG_MAINTAINER='@termux'
+TERMUX_PKG_VERSION='7.6.0'
+TERMUX_PKG_SRCURL=https://github.com/PowerShell/PowerShell/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_SHA256='9970c95bea498a6d30587c2df4885e22f6dbbdf974d57e644046e088434a117d'
