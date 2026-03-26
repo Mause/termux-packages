c506ea4a3349dd926414ed54add7600df76b6068
Elliana May <me@mause.me>
2026-03-26 18:28:25 +0800

    bump(main/tsgo): 7.0.0-dev.20260326

--- packages/tsgo/build.sh
+++ packages/tsgo/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_DESCRIPTION="A port of the typescript compiler to go"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_GIT_BRANCH=main
-_COMMIT_DATE=2026-03-24
+_COMMIT_DATE=2026-03-26
 TERMUX_PKG_VERSION="7.0.0-dev.${_COMMIT_DATE//-/}"
 TERMUX_PKG_HOMEPAGE="https://github.com/microsoft/typescript-go"
 TERMUX_PKG_LICENSE="Apache-2.0"
@@ -9,7 +9,7 @@ TERMUX_PKG_LICENSE_FILE="LICENSE"
 TERMUX_PKG_SRCURL="git+https://github.com/microsoft/typescript-go"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
-_COMMIT="86b0ba896640a56a94f52b102732e5ea029a14ac"
+_COMMIT="267da3a91a410ea0a0510275e889bf0686fc02dc"
 
 set -o xtrace
 
