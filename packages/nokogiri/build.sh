16324c22f263fafb8ab39b9d0e316ca9e8d1e21a
Elliana May <me@mause.me>
2026-04-01 21:43:28 +0800

    tweak path

--- packages/nokogiri/build.sh
+++ packages/nokogiri/build.sh
@@ -22,7 +22,7 @@ termux_step_make_install() {
 	BUNDLE=$GEM_HOME/bin/bundle
 	$GEM_HOME/bin/rake -T
 	ls rbconfig-patch.rb
-	ruby -r ./rbconfig-patch.rb $GEM_HOME/bin/rake compile --trace -- --use-system-libraries || cat "$GEM_HOME/nokogiri-$TERMUX_PKG_VERSION/mkmf.log"
+	ruby -r ./rbconfig-patch.rb $GEM_HOME/bin/rake compile --trace -- --use-system-libraries || cat "$GEM_HOME/nokogiri-$TERMUX_PKG_VERSION/ext/mkmf.log"
 	$BUNDLE config build.nokogiri --use-system-libraries
 	$BUNDLE install --jobs "${TERMUX_PKG_MAKE_PROCESSES}"
 }
