f1bc84e13226464884b3580f298d54285b1cfb72
Elliana May <me@mause.me>
2026-04-01 13:50:49 +0800

    addpkg(main/nokogiri): 1.19.2

---
+++ packages/nokogiri/build.sh
@@ -0,0 +1,9 @@
+TERMUX_PKG_HOMEPAGE='https://github.com/sparklemotion/nokogiri'
+TERMUX_PKG_DESCRIPTION='Nokogiri (鋸) makes it easy and painless to work with XML and HTML from Ruby.'
+TERMUX_PKG_LICENSE='MIT'
+TERMUX_PKG_MAINTAINER='@termux'
+TERMUX_PKG_VERSION='1.19.2'
+TERMUX_PKG_SRCURL=https://github.com/sparklemotion/nokogiri/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_SHA256='e6257d775758b880bacdec1f32876f3a0db4c20b340b9f99540472aab42748cb'
