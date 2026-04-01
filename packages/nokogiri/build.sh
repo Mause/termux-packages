20cae0696b12d2596e9fcbcf8c3105cefff8cbe4
Elliana May <me@mause.me>
2026-04-01 16:12:32 +0800

    downgrade

--- packages/nokogiri/build.sh
+++ packages/nokogiri/build.sh
@@ -2,11 +2,11 @@ TERMUX_PKG_HOMEPAGE='https://github.com/sparklemotion/nokogiri'
 TERMUX_PKG_DESCRIPTION='Nokogiri (鋸) makes it easy and painless to work with XML and HTML from Ruby.'
 TERMUX_PKG_LICENSE='MIT'
 TERMUX_PKG_MAINTAINER='@termux'
-TERMUX_PKG_VERSION='1.19.2'
+TERMUX_PKG_VERSION='1.16.2'
 TERMUX_PKG_SRCURL=https://github.com/sparklemotion/nokogiri/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_SHA256='e6257d775758b880bacdec1f32876f3a0db4c20b340b9f99540472aab42748cb'
+TERMUX_PKG_SHA256='b056af14f2d653146d3c0fdd082fa8c0ae77654ce938066499be5f34cb66a6b5'
 TERMUX_PKG_DEPENDS='ruby, libxslt'
 
 set -o xtrace
