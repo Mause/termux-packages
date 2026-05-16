d8c494d4963ec36eaa2af5ca717dbd039010e593
Elliana May <me@mause.me>
2026-05-16 17:43:53 +0800

    addpkg(main/opentui): 0.2.12

---
+++ packages/opentui/build.sh
@@ -0,0 +1,9 @@
+TERMUX_PKG_HOMEPAGE='https://github.com/anomalyco/opentui'
+TERMUX_PKG_DESCRIPTION='OpenTUI is a library for building terminal user interfaces (TUIs)'
+TERMUX_PKG_LICENSE='MIT'
+TERMUX_PKG_MAINTAINER='@termux'
+TERMUX_PKG_VERSION='0.2.12'
+TERMUX_PKG_SRCURL=https://github.com/anomalyco/opentui/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_SHA256='774cee9adc88a233ee035d035461eb96c50364ef7ee0b3350107b7b3b020043b'
