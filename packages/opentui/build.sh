a779c99ea6a191fb412500452fcbe90bb08954bc
Elliana May <me@mause.me>
2026-05-18 20:35:20 +0800

    bump(main/opentui): 0.2.14

--- packages/opentui/build.sh
+++ packages/opentui/build.sh
@@ -2,11 +2,11 @@ TERMUX_PKG_HOMEPAGE='https://github.com/anomalyco/opentui'
 TERMUX_PKG_DESCRIPTION='OpenTUI is a library for building terminal user interfaces (TUIs)'
 TERMUX_PKG_LICENSE='MIT'
 TERMUX_PKG_MAINTAINER='@termux'
-TERMUX_PKG_VERSION='0.2.12'
+TERMUX_PKG_VERSION='0.2.14'
 TERMUX_PKG_SRCURL=https://github.com/anomalyco/opentui/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_SHA256='774cee9adc88a233ee035d035461eb96c50364ef7ee0b3350107b7b3b020043b'
+TERMUX_PKG_SHA256='6b15806dfa7fbb6f6f8ebb76476b6decff45795859c7381c3be2819115c2732f'
 TERMUX_ZIG_VERSION='0.15.2'
 
 termux_step_pre_configure() {
