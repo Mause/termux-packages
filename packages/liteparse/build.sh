1225e7a68ad740c8de65e72595c63cb6e0f14154
Elliana May <me@mause.me>
2026-06-01 12:14:52 +0800

    download pdfium binary

--- packages/liteparse/build.sh
+++ packages/liteparse/build.sh
@@ -8,8 +8,14 @@ TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_SHA256='33c65f535b68a41fd1512511b1ddebed17c194eaf08df5c207e4edb1d6b83fe9'
 
+set -o xtrace
+
 termux_step_pre_configure() {
 	termux_setup_rust
 	TERMUX_PKG_SRCDIR+="/crates/liteparse"
 	TERMUX_PKG_BUILDDIR="$TERMUX_PKG_SRCDIR"
+
+	wget https://github.com/bblanchon/pdfium-binaries/releases/latest/download/pdfium-android-arm64.tgz
+	tar -vxzf pdfium-android-arm64.tgz
+	PDFIUM_LIB_PATH="$(find . -name "libpdfium.so")"
 }
