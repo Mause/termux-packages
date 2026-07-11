289099b911ca89c495ef7666b6df59cfc3a5e581
Elliana May <me@mause.me>
2026-07-11 13:58:02 +0800

    release version

--- packages/tsgo/build.sh
+++ packages/tsgo/build.sh
@@ -1,22 +1,13 @@
 TERMUX_PKG_DESCRIPTION="A port of the typescript compiler to go"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_GIT_BRANCH=main
-_COMMIT_DATE=2026-07-09
-TERMUX_PKG_VERSION="7.0.0-dev.${_COMMIT_DATE//-/}"
 TERMUX_PKG_HOMEPAGE="https://github.com/microsoft/typescript-go"
 TERMUX_PKG_LICENSE="Apache-2.0"
-TERMUX_PKG_LICENSE_FILE="LICENSE"
-TERMUX_PKG_SRCURL="git+https://github.com/microsoft/typescript-go"
+TERMUX_PKG_HOMEPAGE='https://github.com/microsoft/typescript-go'
+TERMUX_PKG_VERSION='7.0.2'
+TERMUX_PKG_SRCURL=https://github.com/microsoft/typescript-go/archive/refs/tags/typescript/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
-_COMMIT="168e7015edf98244febc8f4ae450b673b5d195d7"
-
-set -o xtrace
-
-termux_post_get_source() {
-	git fetch --unshallow
-	git checkout $_COMMIT
-}
+TERMUX_PKG_SHA256='25cca8ec9d89e4ceec5181a677ded2c6d690fa18b81e1277846303973eb71fcf'
 
 termux_step_pre_configure() {
 	termux_setup_rust
