542f9ea1cd4fa19170d45a3ecb45155eb466d5f9
Elliana May <me@mause.me>
2026-07-16 16:03:06 +0800

    install maturin

--- packages/mitmproxy-rs/build.sh
+++ packages/mitmproxy-rs/build.sh
@@ -8,9 +8,12 @@ TERMUX_PKG_SRCURL=https://github.com/mitmproxy/mitmproxy_rs/archive/refs/tags/v$
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_SHA256='fc20e6f576ab6e5726754555dc96f7753e3580de233f4f911cfbf224bf6dd790'
+TERMUX_PKG_PYTHON_COMMON_BUILD_DEPS="wheel"
 
 termux_step_pre_configure() {
 	termux_setup_rust
+	termux_setup_python_pip
+
 	TERMUX_PKG_SRCDIR+="/mitmproxy-rs"
 	TERMUX_PKG_BUILDDIR+="/mitmproxy-rs"
 }
