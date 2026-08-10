2fd9889e4c151d8f66c25805ef7de4197fb19946
Elliana May <me@mause.me>
2026-08-10 22:20:23 +0800

    patch out arboard

--- packages/worktrunk/build.sh
+++ packages/worktrunk/build.sh
@@ -10,4 +10,19 @@ TERMUX_PKG_SHA256='984936d68f7a47afabc8670c2f7433da2f41b4815032ab472d6ced67993c3
 
 termux_step_pre_configure() {
 	termux_setup_rust
+
+	cargo vendor
+	find ./vendor \
+		-mindepth 1 -maxdepth 1 -type d \
+		! -wholename ./vendor/arboard \
+		-exec rm -rf '{}' \;
+
+	find vendor/arboard -type f -print0 | \
+		xargs -0 sed -i \
+		-e 's|android|disabling_this_because_it_is_for_building_an_apk|g' \
+		-e "s|/tmp|$TERMUX_PREFIX/tmp|g"
+
+	echo "" >> Cargo.toml
+	echo '[patch.crates-io]' >> Cargo.toml
+	echo "arboard = { path = \"./vendor/arboard\" }" >> Cargo.toml
 }
